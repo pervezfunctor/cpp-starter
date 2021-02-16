95a748f0cc2f815cd743e4ecd52e3b914bfe625d
Pervez Iqbal <pervezfunctor@gmail.com>
2021-02-16 10:10:39 +0000

    improve configuration

--- common.hpp
+++ common.hpp
@@ -1,7 +1,20 @@
 #include <iostream>
+
 #include <vector>
 #include <list>
+#include <set>
+#include <map>
+#include <array>
+
 #include <cassert>
 #include <iterator>
 #include <algorithm>
 #include <type_traits>
+#include <memory>
+#include <numeric>
+#include <ranges>
+
+#include <tuple>
+#include <any>
+#include <variant>
+#include <optional>
